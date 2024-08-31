6b8444d149a59450bc23e894702206f66b54384c
KOSASIH <kosasihg88@gmail.com>
2024-08-31 20:36:42 +0700

    Create init.py

---
+++ content-generation/init.py
@@ -0,0 +1,12 @@
+"""
+Content Generation Module
+
+This module is responsible for generating high-quality content related to cryptocurrency and blockchain topics using advanced AI and machine learning techniques.
+"""
+
+from .content_generator import ContentGenerator
+from .content_templates import ContentTemplates
+from .language_models import LanguageModels
+from .post_processing import PostProcessing
+
+__all__ = ["ContentGenerator", "ContentTemplates", "LanguageModels", "PostProcessing"]
